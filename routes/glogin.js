539ff94e126e3cc58ebdbd1fe1feefd732872ada
tymoste <tymoste@gmail.com>
2024-05-19 21:35:08 +0200

    Fixed again

--- routes/glogin.js
+++ routes/glogin.js
@@ -11,48 +11,51 @@ router.get('/', (req, res) => {
           access_type: 'offline',
           scope: 'https://www.googleapis.com/auth/userinfo.profile'
       });
-      console.log(url)
+      console.log(url);
       res.redirect(url);
   } else {
       const oauth2 = google.oauth2({ version: 'v2', auth: req.app.locals.oAuth2Client });
-      oauth2.userinfo.v2.me.get(function (err, result){
-        if(err){
-            console.log("Error:")
-            console.log(err)
-        }else{
-            loggedUser = result.data.name
-            console.log(loggedUser)
+      oauth2.userinfo.v2.me.get(function (err, result) {
+        if (err) {
+            console.log("Error:");
+            console.log(err);
+            res.send('Error retrieving user info');
+        } else {
+            const loggedUser = result.data.name;
+            console.log(loggedUser);
+            res.send(`Logged in: ${loggedUser} <img src="${result.data.picture}" height="23" width="23"> <a href="/logout"> Log me out </a>`);
         }
-        res.send('Logged in: '.concat(loggedUser, ' <img src="', result.data.picture, '"height="23" width="23">', '<a href = "/"> Log me out </a>'))
       });
   }
-})
+});
 
 router.get('/auth/google/callback', function (req, res) {
-  const code = req.query.code
+  const code = req.query.code;
   if (code) {
       // Get an access token based on our OAuth code
       req.app.locals.oAuth2Client.getToken(code, function (err, tokens) {
           if (err) {
-              console.log('Error authenticating')
+              console.log('Error authenticating');
               console.log(err);
+              res.send('Error during authentication');
           } else {
               console.log('Successfully authenticated');
               req.app.locals.oAuth2Client.setCredentials(tokens);
               req.app.locals.authed = true;
-              res.redirect('/glogin')
+              res.redirect('/glogin');
           }
       });
   }
 });
 
 router.get('/logout', (req, res) => {
-    req.app.locals.oAuth2Client.revokeCredentials(function(err, result) {
+    req.app.locals.oAuth2Client.revokeCredentials(function (err, result) {
         if (err) {
             console.error('Error revoking credentials:', err);
+            res.send('Error during logout');
         } else {
             console.log('Credentials revoked successfully.');
-            authed = false;
+            req.app.locals.authed = false; // Ustawienie authed na false po wylogowaniu
             res.redirect('/');
         }
     });
