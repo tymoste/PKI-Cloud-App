3944572251b7a12a6113410f667b50a8c5cb7a6a
tymoste <tymoste@gmail.com>
2024-06-16 22:49:02 +0200

    Another fix

--- routes/glogin.js
+++ routes/glogin.js
@@ -10,50 +10,56 @@ const gitClientId = 'Ov23liVDoUk6114fGsAy';
 const gitClientSecret = '3b98c6bf5b82dbe0ccef8b991b04222530a99e50';
     
 /* GET home page. */
-router.get('/', (req, res) => {
-  if (!req.app.locals.authed) {
-      // Generate an OAuth URL and redirect there
-      const url = req.app.locals.oAuth2Client.generateAuthUrl({
-          access_type: 'offline',
-          scope: 'https://www.googleapis.com/auth/userinfo.profile'
-      });
-      console.log(url);
-      res.redirect(url);
-  } else {
-      const oauth2 = google.oauth2({ version: 'v2', auth: req.app.locals.oAuth2Client });
-      oauth2.userinfo.v2.me.get(function (err, result) {
-        if (err) {
-            console.log("Error:");
-            console.log(err);
-            res.send('Error retrieving user info');
-        } else {
-            const loggedUser = result.data.name;
-            console.log(loggedUser);
+router.get('/', async (req, res) => {
+    if (!req.app.locals.authed) {
+        // Generate an OAuth URL and redirect there
+        const url = req.app.locals.oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: 'https://www.googleapis.com/auth/userinfo.profile'
+        });
+        console.log(url);
+        res.redirect(url);
+    } else {
+        const oauth2 = google.oauth2({ version: 'v2', auth: req.app.locals.oAuth2Client });
+        oauth2.userinfo.v2.me.get(async function (err, result) {
+            if (err) {
+                console.log("Error:");
+                console.log(err);
+                res.send('Error retrieving user info');
+            } else {
+                const loggedUser = result.data.name;
+                console.log(loggedUser);
 
-            const connectDB = async() => {
-                try{
-                    const client = new Client({
-                        user: process.env.PGUSER,
-                        host: process.env.PGHOST,
-                        database: process.env.PGDATABASE,
-                        password: process.env.PGPASSWORD,
-                        port: process.env.PGPORT
-                    })
-            
-                    await client.connect()
-                    const result2 = await client.query("SELECT * FROM users")
-                    await client.end();
-            
-                }catch(error){
-                    console.log(error);
+                const connectDB = async () => {
+                    try {
+                        const client = new Client({
+                            user: process.env.PGUSER,
+                            host: process.env.PGHOST,
+                            database: process.env.PGDATABASE,
+                            password: process.env.PGPASSWORD,
+                            port: process.env.PGPORT
+                        });
+
+                        await client.connect();
+                        const result2 = await client.query("SELECT * FROM users");
+                        await client.end();
+
+                        return result2.rows;
+                    } catch (error) {
+                        console.log(error);
+                        return [];
+                    }
+                };
+
+                try {
+                    const queryResult = await connectDB();
+                    res.render("gitloginsuccess", { userData: result.data, queryResult: queryResult });
+                } catch (error) {
+                    res.send('Error retrieving data');
                 }
-                res.render("gitloginsuccess", {userData:result.data, qureyResult: result2.rows})
             }
-            connectDB();
-            // res.send(`Logged in: ${loggedUser} <img src="${result.data.picture}" height="23" width="23"> <a href="/glogin/logout"> Log me out </a>`);
-        }
-      });
-  }
+        });
+    }
 });
 
 router.get('/auth/google/callback', function (req, res) {
